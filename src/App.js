a509a662eba3ece7501de60b89889bd232612842
Elijha Couch <31853975+ElijhaC@users.noreply.github.com>
2023-07-11 23:38:15 -0400

    Added category styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './categories.styles.scss';
 
 const App = () => {
 
@@ -21,20 +22,20 @@ const App = () => {
     {
       id: 5,
       title: 'Mens',
-    }
-  ]
+    },
+  ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => {
+      {categories.map(({ title }) => (
         <div className="category-container">
-           {/* <img />> */}
+           <div className="background-img" />
            <div className="category-body-container">
              <h2>{title}</h2>
              <p>Shop Now</p>
            </div>
         </div>
-      })}
+      ))}
     </div>
   );
 };
